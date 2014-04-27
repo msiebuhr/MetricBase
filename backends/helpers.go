0a0fc627da210bddbc6e4b5f442d8040ea5e48f7
Morten Siebuhr <sbhr@sbhr.dk>
2014-04-27 13:23:08 +0200

    Backends: Document helper functions.

--- backends/helpers.go
+++ backends/helpers.go
@@ -4,6 +4,8 @@ import (
 	"github.com/msiebuhr/MetricBase"
 )
 
+// GetMetricsAsList fetches all the metrics in a backend and returns it as a
+// list.
 func GetMetricsAsList(backend MetricBase.Backend) []string {
 	list := make([]string, 0)
 	out := make(chan string)
@@ -15,6 +17,7 @@ func GetMetricsAsList(backend MetricBase.Backend) []string {
 	return list
 }
 
+// GetDataAsList fetches the relevant data and returns it as a list.
 func GetDataAsList(backend MetricBase.Backend, name string, from, to int64) []MetricBase.MetricValues {
 	list := make([]MetricBase.MetricValues, 0)
 	out := make(chan MetricBase.MetricValues)
