a7d25c792137f2ebacd3ce80366f8634ef0024f8
Morten Siebuhr <sbhr@sbhr.dk>
2014-05-11 21:12:54 +0200

    backends testProxy: Generalize data generators.

--- backends/testProxy.go
+++ backends/testProxy.go
@@ -7,6 +7,30 @@ import (
 	"github.com/msiebuhr/MetricBase"
 )
 
+var patterns map[string]func(i int64) float64
+
+func init() {
+	patterns = make(map[string]func(i int64) float64)
+
+	// Basic math stuff
+	patterns["sin.hour"] = func(i int64) float64 {
+		delta := 60.0 * 60
+		return math.Sin(2 * math.Pi / delta * float64(i))
+	}
+	patterns["sin.day"] = func(i int64) float64 {
+		delta := 60.0 * 60 * 24
+		return math.Sin(2 * math.Pi / delta * float64(i))
+	}
+	patterns["sin.week"] = func(i int64) float64 {
+		delta := 60.0 * 60 * 24 * 7
+		return math.Sin(2 * math.Pi / delta * float64(i))
+	}
+
+	// Constants
+	patterns["const.1"] = func(i int64) float64 { return 1.0 }
+
+}
+
 // TestProxy sits in front of another backend and services various kinds of
 // dummy data under the 'test.'-prefix.
 type TestProxy struct {
@@ -26,11 +50,13 @@ func (tp *TestProxy) AddMetric(metric MetricBase.Metric) {
 
 func (tp *TestProxy) GetMetricsList(results chan string) {
 	go func() {
-		//defer close(results)
 		// Inject known test metrics
-		results <- "test.sin"
+		for name := range patterns {
+			results <- "test." + name
+		}
 
 		// Ask whatever backend we're running on about the rest
+		// Don't close results, as callee will do that:w
 		tp.nextBackend.GetMetricsList(results)
 	}()
 }
@@ -44,12 +70,8 @@ func (tp *TestProxy) GetRawData(name string, from, to int64, result chan MetricB
 	// Default function to use for generating data
 	f := func(i int64) float64 { return float64(i) }
 
-	// Switch on what the rest of the name is
-	switch name[5:] {
-	case "sin":
-		f = func(i int64) float64 {
-			return math.Sin(float64(i))
-		}
+	if fun, ok := patterns[name[5:]]; ok {
+		f = fun
 	}
 
 	// Run the generator-function on relevant input
