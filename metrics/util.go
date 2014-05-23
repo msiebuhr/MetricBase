f0eb2d609a996eff5489375157d79979bef2bd64
Morten Siebuhr <sbhr@sbhr.dk>
2014-05-23 22:46:57 +0200

    Metrics: Add simple helpers to encode/decode timestamps to []byte.

---
+++ metrics/util.go
@@ -0,0 +1,27 @@
+package metrics
+
+import (
+	"time"
+)
+
+// TimeToUint40 encodes a Unix-timestamp with second-resolution and
+// encodes it in five bytes.
+//
+// Behavior is undefined for dates before 1970 and somewhere after year 2300.
+func TimeToUint40(b []byte, t time.Time) {
+	v := uint64(t.Unix())
+	b[0] = byte(v >> 32)
+	b[1] = byte(v >> 24)
+	b[2] = byte(v >> 16)
+	b[3] = byte(v >> 8)
+	b[4] = byte(v)
+}
+
+func Uint4ToTime(b []byte) time.Time {
+	i := int64(b[4]) |
+		int64(b[3])<<8 |
+		int64(b[2])<<16 |
+		int64(b[1])<<24 |
+		int64(b[0])<<32
+	return time.Unix(i, 0)
+}
---
+++ metrics/util_test.go
@@ -0,0 +1,77 @@
+package metrics
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTimeToUint40(t *testing.T) {
+	var tests = []struct {
+		in  time.Time
+		out []byte
+	}{
+		// What about zero
+		{time.Unix(0, 0), []byte{0, 0, 0, 0, 0}},
+
+		// Discards milliseconds
+		{time.Unix(1, 1), []byte{0, 0, 0, 0, 1}},
+
+		// Larger numbers
+		{time.Unix(255, 1), []byte{0, 0, 0, 0, 0xff}},
+		{time.Unix(1<<8, 1), []byte{0, 0, 0, 1, 0}},
+		{time.Unix(1<<16, 1), []byte{0, 0, 1, 0, 0}},
+		{time.Unix(1<<24, 1), []byte{0, 1, 0, 0, 0}},
+		{time.Unix(1<<32, 1), []byte{1, 0, 0, 0, 0}},
+
+		// Shoot over
+		{time.Unix(1<<40, 1), []byte{0, 0, 0, 0, 0}},
+	}
+
+	for _, tt := range tests {
+		outArray := make([]byte, 5)
+		TimeToUint40(outArray, tt.in)
+
+		if len(outArray) != len(tt.out) ||
+			outArray[0] != tt.out[0] ||
+			outArray[1] != tt.out[1] ||
+			outArray[2] != tt.out[2] ||
+			outArray[3] != tt.out[3] ||
+			outArray[4] != tt.out[4] {
+			t.Errorf("Expected %v to encode to %v, got %v.", tt.in, tt.out, outArray)
+		}
+	}
+}
+
+func BenchmarkTimeToUint40(b *testing.B) {
+	out := make([]byte, 5)
+	t := time.Unix(int64(b.N), 0)
+	for i := 0; i < b.N; i++ {
+		TimeToUint40(out, t)
+	}
+}
+
+func TestUint4ToTime(t *testing.T) {
+	var tests = []struct {
+		in  []byte
+		out time.Time
+	}{
+		{[]byte{0, 0, 0, 0, 0}, time.Unix(0, 0)},
+
+		{[]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF}, time.Date(36812, 2, 20, 0, 36, 15, 0, time.UTC)},
+	}
+
+	for _, tt := range tests {
+		res := Uint4ToTime(tt.in)
+
+		if !res.Equal(tt.out) {
+			t.Errorf("Expected %v to decode to %v, got %v.", tt.in, tt.out, res)
+		}
+	}
+}
+
+func BenchmarkUint40ToTime(b *testing.B) {
+	in := make([]byte, 5)
+	for i := 0; i < b.N; i++ {
+		_ = Uint4ToTime(in)
+	}
+}
