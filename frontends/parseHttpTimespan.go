d0df2f259a532b9739d8299e779b99838c5a463f
Morten Siebuhr <sbhr@sbhr.dk>
2014-05-26 23:14:09 +0200

    HTTP frontend: More flexible time parsing.

--- frontends/parseHttpTimespan.go
+++ frontends/parseHttpTimespan.go
@@ -2,37 +2,81 @@ package frontends
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 )
+
+// Parse an interval and give start and end estimates
+func parseInterval(interval string) (time.Time, time.Time, error) {
+	// If it's all digits - YYYYMMDD, YYYYMM or YYYY
+	_, err := strconv.ParseInt(interval, 10, 32)
+	if err == nil && (len(interval) == 8 || len(interval) == 6 || len(interval) == 4) {
+		startMonth := time.January
+		endMonth := time.December
+		startDay := 1
+		endDay := 31
+
+		// There's always a year
+		year, _ := strconv.ParseInt(interval[0:4], 10, 32)
+		// endDay = 31 // Always ends on december 31st
+
+		if len(interval) >= 6 {
+			month, _ := strconv.ParseInt(interval[4:6], 10, 32)
+			startMonth = time.Month(month)
+			endMonth = time.Month(month)
+			// TODO: Figure out last day of last month
+		}
+		if len(interval) == 8 {
+			day, _ := strconv.ParseInt(interval[6:8], 10, 32)
+			startDay = int(day)
+			endDay = int(day)
+		}
+
+		return time.Date(int(year), startMonth, startDay, 0, 0, 0, 0, time.UTC), time.Date(int(year), endMonth, endDay, 23, 59, 59, 0, time.UTC), nil
+	}
+
+	// The initial ParseInt returned some error that isn't about syntax
+	if err != nil && err != strconv.ErrSyntax {
+		return time.Time{}, time.Time{}, err
+	}
+
+	// Relative - ex. -1w
+	duration, err := parseDuration(interval)
+	if err != nil {
+		return time.Time{}, time.Time{}, err
+	}
+
+	// Flip duration so it always goes back in time
+	if duration > 0 {
+		duration = -duration
+	}
+
+	return time.Now().Add(duration), time.Now(), nil
+}
 
 func ParseHttpTimespan(req *http.Request) (time.Time, time.Time, error) {
 	intervalString := req.FormValue("interval")
 
 	if intervalString != "" {
-		// Parse stuff line "-1w1d"
-		duration, err := parseDuration(intervalString)
-		if err != nil {
-			return time.Time{}, time.Time{}, err
-		}
-
-		if duration > 0 {
-			duration = -duration
-		}
-		return time.Now().Add(duration), time.Now(), nil
+		return parseInterval(intervalString)
 	}
 
 	startString := req.FormValue("start")
 	endString := req.FormValue("end")
 
-	startDuration, err := parseDuration(startString)
+	startTime, _, err := parseInterval(startString)
 	if err != nil {
 		return time.Time{}, time.Time{}, err
 	}
 
-	endDuration, err := parseDuration(endString)
+	_, endTime, err := parseInterval(endString)
 	if err != nil {
 		return time.Time{}, time.Time{}, err
 	}
 
-	return time.Now().Add(startDuration), time.Now().Add(endDuration), nil
+	if endTime.Before(startTime) {
+		endTime, startTime = startTime, endTime
+	}
+
+	return startTime, endTime, nil
 }
---
+++ frontends/parseHttpTimespan_test.go
@@ -0,0 +1,70 @@
+package frontends
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+)
+
+func TestParseHttpTimespan_Table(t *testing.T) {
+	// Generate requests and expected output
+
+	var timespan_tests = []struct {
+		queryString string
+		start, end  time.Time
+	}{
+		// Intervals
+		{"interval=2014",
+			time.Date(2014, 1, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2014, 12, 31, 23, 59, 59, 0, time.UTC)},
+		{"interval=201405",
+			time.Date(2014, 5, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2014, 5, 31, 23, 59, 59, 0, time.UTC)},
+		{"interval=20140526",
+			time.Date(2014, 5, 26, 0, 0, 0, 0, time.UTC),
+			time.Date(2014, 5, 26, 23, 59, 59, 0, time.UTC)},
+
+		// Start and end times
+		{"start=2013&end=2014",
+			time.Date(2013, 1, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2014, 12, 31, 23, 59, 59, 0, time.UTC)},
+		{"start=201405&end=20140526",
+			time.Date(2014, 5, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2014, 5, 26, 23, 59, 59, 0, time.UTC)},
+
+		/*
+			// TODO: Tests with relative timestamps
+			{"start=-1w&end=-1h",
+				time.Date(2014, 5, 1, 0, 0, 0, 0, time.UTC),
+				time.Date(2014, 5, 26, 23, 59, 59, 0, time.UTC)},
+			// TODO: Tests with months that doesn't have 31 days
+			{"interval=201402",
+				time.Date(2014, 2, 1, 0, 0, 0, 0, time.UTC),
+				time.Date(2014, 2, 28, 23, 59, 59, 0, time.UTC)},
+			// TODO: Leap years
+		*/
+	}
+
+	for _, tt := range timespan_tests {
+		url, _ := url.Parse("/rpc/query?" + tt.queryString)
+		req := &http.Request{
+			URL: url,
+		}
+		req.ParseForm()
+
+		start, end, err := ParseHttpTimespan(req)
+		if err != nil {
+			t.Errorf("Got unexpected error parsing %s: %s", tt.queryString, err)
+			continue
+		}
+
+		if !start.Equal(tt.start) {
+			t.Errorf("Expected %v to start at %v, but got %v.", tt.queryString, tt.start, start)
+		}
+
+		if !end.Equal(tt.end) {
+			t.Errorf("Expected %v to end at %v, but got %v.", tt.queryString, tt.end, end)
+		}
+	}
+}
